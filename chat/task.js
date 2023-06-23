13c475ecbb04d763bd474a8dabb9e425d7969115
Yury Vereskun <satriks@mail.ru>
2023-06-23 15:53:33 +0300

    simple chat done

---
+++ chat/task.js
@@ -0,0 +1,46 @@
+const answerList = [ 
+    'ok',
+    'Мы ответим Вам потом',
+    'Так не можем',
+    'Да, именно так',
+    'Что то ответим',
+]
+
+const chatWindow = document.querySelector('.chat-widget__messages')
+const message = document.querySelector('input')
+message.addEventListener('keydown', getMessage)
+const chat = document.querySelector('.chat-widget')
+document.querySelector('.chat-widget__side').addEventListener('click', () => {
+    chat.classList.toggle('chat-widget_active')
+})
+
+
+function getMessage(event) {
+    if (event.key == 'Enter'){
+        if (message.value) {
+            let date = new Date()
+            chatWindow.innerHTML += `Сообщение от робота
+            <div class="message message_client">
+                <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
+                <div class="message__text">${message.value}</div>
+            </div>
+            `
+
+            chatWindow.innerHTML +=`Сообщение от клиента
+            <div class="message">
+                <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
+                <div class="message__text">${getAnswer()}</div>
+            </div>
+            `
+        }
+        message.value = ''
+       
+    }
+
+    
+}
+
+function getAnswer() {
+    return answerList[Math.floor(Math.random() * 10) % answerList.length]
+    
+}
