1f50a3a43b5c166bf05d05db4fa15f31a76f56b5
Yury Vereskun <satriks@mail.ru>
2023-06-23 16:17:30 +0300

    chat done all task

--- chat/task.js
+++ chat/task.js
@@ -1,3 +1,8 @@
+
+(function () {
+    
+
+
 const answerList = [ 
     'ok',
     'Мы ответим Вам потом',
@@ -10,9 +15,11 @@ const chatWindow = document.querySelector('.chat-widget__messages')
 const message = document.querySelector('input')
 message.addEventListener('keydown', getMessage)
 const chat = document.querySelector('.chat-widget')
+let myTimer;
 
 document.querySelector('.chat-widget__side').addEventListener('click', () => {
     chat.classList.toggle('chat-widget_active')
+    let myTimer = setTimeout(timerQuestion, 30000)
 })
 
 
@@ -20,14 +27,14 @@ function getMessage(event) {
     if (event.key == 'Enter'){
         if (message.value) {
             let date = new Date()
-            chatWindow.innerHTML += `Сообщение от робота
+            chatWindow.innerHTML += `Сообщение от клиента
             <div class="message message_client">
                 <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
                 <div class="message__text">${message.value}</div>
             </div>
             `
 
-            chatWindow.innerHTML +=`Сообщение от клиента
+            chatWindow.innerHTML +=`Сообщение от робота
             <div class="message">
                 <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
                 <div class="message__text">${getAnswer()}</div>
@@ -35,6 +42,11 @@ function getMessage(event) {
             `
         }
         chatCont.scrollTop = chatCont.scrollTopMax
+        console.log(myTimer);
+        if (myTimer){
+             window.clearTimeout(myTimer)
+        }
+        myTimer = setTimeout(timerQuestion , 30000) 
 
         message.value = ''
        
@@ -47,3 +59,16 @@ function getAnswer() {
     return answerList[Math.floor(Math.random() * 10) % answerList.length]
     
 }
+
+function timerQuestion() {
+    let date = new Date()
+    chatWindow.innerHTML +=`Сообщение от робота
+    <div class="message">
+        <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
+        <div class="message__text">${getAnswer()}</div>
+    </div>
+    `
+    chatCont.scrollTop = chatCont.scrollTopMax
+}
+
+}) ()
